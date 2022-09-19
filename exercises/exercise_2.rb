2f77cbae79fe2b79fc67a9b673d7777e7e01a242
Jeffrey <jeff.ho@hotmail.com>
2022-09-19 17:19:33 -0400

    Add employees to employee table

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -9,6 +9,7 @@ puts "----------"
 @store1 = Store.find_by(id: 1)
 @store2 = Store.find_by(id: 2)
 
+
 puts "Store 1 is called #{@store1.name}"
 @store1.name = "Toronto"
 puts "Store 1's new name is #{@store1.name}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,4 +10,16 @@ puts "----------"
 
 # Your code goes here ...
 
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 40)
+@store1.employees.create(first_name: "Billy", last_name: "Kimbur", hourly_rate: 15)
 
+@store2.employees.create(first_name: "Sasha", last_name: "Pattuci", hourly_rate: 80)
+@store2.employees.create(first_name: "Arnold", last_name: "Cantuk", hourly_rate: 35)
+@store2.employees.create(first_name: "Olivia", last_name: "Mansel", hourly_rate: 50)
+
+@employees = Employee.all
+
+@employees.each do |employee| 
+  p employee.first_name
+end
